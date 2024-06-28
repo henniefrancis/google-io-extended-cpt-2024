eab0f84438ba9088224f04a7082ccb2637456242
Hennie Francis <henniefrancis@gmail.com>
2024-06-28 23:24:53 +0200

    outputs removed

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "storage_bucket" {
-  value = google_storage_bucket.google-io-extended-cpt-2024-google-storage-bucket
-}
+# output "storage_bucket" {
+#   value = google_storage_bucket.google-io-extended-cpt-2024-google-storage-bucket
+# }
