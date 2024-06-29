35560e1418d5f9330af928ae322ca28935008128
Hennie Francis <henniefrancis@users.noreply.github.com>
2024-06-29 12:20:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,18 +9,18 @@ locals {
   bucket_name = "g-io-extended-2024-bucket"
 }
 
-# resource "google_storage_bucket" "google-io-extended-cpt-2024-google-storage-bucket" {
-#   name                        = local.bucket_name
-#   location                    = "US"
-#   storage_class               = "STANDARD"
-#   uniform_bucket_level_access = true
-#   public_access_prevention    = "enforced"
-#   force_destroy               = true
-#   project                     = var.google_project.googleioextendedcpt2024
-#   labels = merge(var.default_tags,
-#     {
-#       cloudprovider = "google-cloud"
-#       event         = "google-io-extended-2024-bucket"
-#       location      = "cape-town"
-#   })
-# }
+ resource "google_storage_bucket" "google-io-extended-cpt-2024-google-storage-bucket" {
+   name                        = local.bucket_name
+   location                    = "US"
+   storage_class               = "STANDARD"
+   uniform_bucket_level_access = true
+   public_access_prevention    = "enforced"
+   force_destroy               = true
+   project                     = var.google_project.googleioextendedcpt2024
+   labels = merge(var.default_tags,
+     {
+       cloudprovider = "google-cloud"
+       event         = "google-io-extended-2024-bucket"
+       location      = "cape-town"
+   })
+ }
